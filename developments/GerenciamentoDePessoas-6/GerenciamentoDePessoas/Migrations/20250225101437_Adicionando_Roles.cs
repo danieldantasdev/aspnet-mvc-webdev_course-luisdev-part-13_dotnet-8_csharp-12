376b83dd68119a3571f9496f8941ee0709b474ba
Daniel Dantas <contatodanieldantasdev@gmail.com>
2025-12-01 01:29:47 -0300

    feat: adição do arquivo 20250225101437_Adicionando_Roles.cs

---
+++ developments/GerenciamentoDePessoas-6/GerenciamentoDePessoas/Migrations/20250225101437_Adicionando_Roles.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace GerenciamentoDePessoas.Migrations
+{
+    /// <inheritdoc />
+    public partial class Adicionando_Roles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[,]
+                {
+                    { "1A7E2F64-9A1E-4A9E-9B57-2F3028E3A02D", null, "administrador", "ADMINISTRADOR" },
+                    { "2B9E3F75-ABCD-4D5E-8C56-3D1238E4B03E", null, "operador", "OPERADOR" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "1A7E2F64-9A1E-4A9E-9B57-2F3028E3A02D");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "2B9E3F75-ABCD-4D5E-8C56-3D1238E4B03E");
+        }
+    }
+}
